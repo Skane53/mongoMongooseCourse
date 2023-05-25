5d7aff9b207f6bef18cadcb1f5773c3a06231d76
Samba Kane <sambakane175@gmail.com>
2023-05-25 16:06:38 +0000

    Find and Update

--- myApp.js
+++ myApp.js
@@ -67,7 +67,15 @@ const findPersonById = (personId, done) => {
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
 
-  done(null /*, data*/);
+  Person.findById({ _id: personId }, (err, person) => {
+    if (err) return console.log(err);
+    person.favoriteFoods.push(foodToAdd);
+
+    person.save((err, updatedPerson) => {
+      if (err) return console.log(err);
+      done(null, updatedPerson);
+    });
+  });
 };
 
 const findAndUpdate = (personName, done) => {
